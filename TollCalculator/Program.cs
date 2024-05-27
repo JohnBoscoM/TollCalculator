0e8676120a1b6d28d859fa34eca56d8d8c92bd8a
Bosco <john.matanda@outlook.com>
2024-05-27 13:54:59 +0200

    Add project files.

---
+++ TollCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TollFeeCalculator;
+
+TollCalculator tollCalculator = new TollCalculator();
+
+DateTime date = DateTime.Now; 
+Vehicle vehicle = new Car(); 
+int fee = tollCalculator.GetTollFee(date, vehicle);
+
+// Print the result
+Console.WriteLine($"The toll fee is: {fee}");
