ac9efc8ba89f013f49070a708a12689b9a42352c
Bosco <john.matanda@outlook.com>
2024-05-31 14:11:11 +0200

    Added Unit test to test driver is not charged if it is a toll free date or vehicle is toll free.

---
+++ TollFeeCalculator.Test/TollFeeCalculatorTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using TollFeeCalculator;
+using System.Runtime.ConstrainedExecution;
+
+namespace TollFeeCalculator.Tests
+{
+    public class TollCalculatorTests
+    {
+        private readonly ITollFeeSchedule _tollFeeSchedule = new TollFeeSchedule();
+        private readonly ITollFreeDate _tollFreeDate = new TollFreeDate();
+        private readonly TollCalculator _tollCalculator;
+
+        public TollCalculatorTests()
+        {
+            _tollCalculator = new TollCalculator(_tollFreeDate, _tollFeeSchedule);
+        }
+
+        [Fact]
+        public void Vehicle_IsTollFree_ShouldReturnZeroFee()
+        {
+            Vehicle vehicle = new Motorbike();
+            DateTime[] dates = { DateTime.Today.AddHours(7) };
+
+            int fee = _tollCalculator.GetTollFee(vehicle, dates);
+
+            Assert.Equal(0, fee);
+        }
+
+        [Fact]
+        public void TollFreeDate_ShouldReturnZeroFee()
+        {
+            Vehicle vehicle = new Car();
+            DateTime[] dates = { new DateTime(2024, 1, 1, 7, 0, 0) };
+
+            int fee = _tollCalculator.GetTollFee(vehicle, dates);
+
+            Assert.Equal(0, fee);
+        }
+    }
+}
